78c1320b7cf40f69f45230257858da7dff70cb95
Nadia Begum <76848926+Nadia-Begum@users.noreply.github.com>
2024-07-25 15:49:02 +0100

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Nadia Begum on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
